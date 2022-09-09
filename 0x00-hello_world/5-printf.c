5fbea8095ec5d965dc5add2533e7b9e81960d38d
Abdullahi Ibrahim <aismukaddas@gmail.com>
2022-09-08 18:53:40 -0700

    Task 4-8 L-C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>                                                    
+
+                                                                      
+
+/**                                                                   
+ *
+ *  * main - Print a test                                                  
+ *
+ *   *                                                                    
+ *
+ *    * Return: Always 0 (Success)                                    
+ *
+ *     */                                                                   
+
+int main(void)                                                       
+
+{                                                                     
+
+	        puts("\"Programming is like building a multilingual puzzle"); 
+
+		        return (0);                                                   
+
+}         
