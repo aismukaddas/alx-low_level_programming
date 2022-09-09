d15a8092bbd1d883b69733fc6d69e1a789f770cf
Abdullahi Ibrahim <aismukaddas@gmail.com>
2022-09-08 19:38:03 -0700

    Task 5 correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,14 @@
-#include <stdio.h>                                                    
+#include <stdio.h>
 
-                                                                      
-
-/**                                                                   
- *
- *  * main - Print a test                                                  
- *
- *   *                                                                    
- *
- *    * Return: Always 0 (Success)                                    
- *
- *     */                                                                   
-
-int main(void)                                                       
-
-{                                                                     
-
-	        puts("\"Programming is like building a multilingual puzzle"); 
-
-		        return (0);                                                   
-
-}         
+/**
+* main - print the string in the put function
+*
+Â* Description: using the main function
+* this program prints "Programming is like a building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     return (0);
+}
