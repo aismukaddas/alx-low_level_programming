c896090da4ab28b024defa74becc43bf16124f6d
Abdullahi Ibrahim <aismukaddas@gmail.com>
2022-09-09 22:09:11 -0700

    The success combination in business is

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,18 +5,16 @@
  *
  * Return: returns zero at the end
  */
-int main (void)
+
+int main(void)
 {
-	int i, p, m;
+		int i, p, m;
 	for (i = 0; i <= 9; i++)
 	{
 		for (p = 1; p <= 9; p++)
 		{
-
-			for (m = 2; m <= 9; m++)
-
+		for (m = 2; m <= 9; m++)
 			{
-
 			if (m > p && p > i)
 			{
 				putchar(i + '0');
