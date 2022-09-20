795caffe40b8880861a6530903069f3b956ae832
Abdullahi Ibrahim <aismukaddas@gmail.com>
2022-09-20 03:22:58 +0000

    Multi Tasks

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
-*reset_to_98 - resets the value of given integer to 98
-*@n: parameter
-*Return : nothing
+*swap_int - swaps two integers' values
+*@a: first integer
+*@b: second integer
+*Return: returns nothing
 */
 
-void reset_to_98(int *n)
+void swap_int(int *a, int *b)
 {
-	*n = 98;
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
 }
