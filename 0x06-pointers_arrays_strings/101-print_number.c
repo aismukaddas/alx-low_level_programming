2663d86aebadcc3db1f521ebbea0de0d09a82e48
Abdullahi Ibrahim <aismukaddas@gmail.com>
2022-09-23 03:24:55 +0000

    Correction

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,83 +1,25 @@
-Vmain.h.txt
-Who has access
-A
-System properties
-Type
-Text
-Size
-637 bytes
-Storage used
-637 bytes
-Location
-0x06-pointers_arrays_strings
-Owner
-Antony Bahati
-Modified
-5:27 PM by Antony Bahati
-Opened
-4:44 AM by me
-Created
-5:27 PM
-No description
-Viewers can download
-#include "main.h"
-#include <stdio.h>
-
-/**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
- *
- * Return: void
- */
-
-void print_line(char *c, int s, int l)
-{
-	int j, k;
-
-	for (j = 0; j <= 9; j++)
-	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-	}
+#include <stdio.h>                                                                                                                    
+                                                                                                                                      
+int main(void)                                                                                                                        
+{                                                                                                                                     
+        int n;                                                                                                                        
+        int a[5];                                                                                                                     
+        int *p;                                                                                                                       
+                                                                                                                                      
+        a[2] = 1024;                                                                                                                  
+        p = &n;                                                                                                                       
+/*                                                                                                                                    
+ * write your line of code here...                                                                                                    
+ * Remember:                                                                                                                          
+ * main - magic                                                                                                                       
+ * - you are not allowed to use a                                                                                                     
+ *   - you are not allowed to modify p                                                                                                
+ *   - only one statement                                                                                                             
+ *   - you are not allowed to code anything else than this line of code                                                               
+ */                                                                                                                                   
+        *(p + 5) = 98;                                                                                                                
+/* ...so that this prints 98\n */                                                                                                     
+        printf("a[2] = %d\n", a[2]);                                                                                                  
+        return (0);                                                                                                                   
 }
 
-/**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
- *
- * Return: void
- */
-void print_buffer(char *b, int size)
-{
-	int i;
-
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
-	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
-		putchar('\n');
-	}
-	if (size == 0)
-		putchar('\n');
-}
