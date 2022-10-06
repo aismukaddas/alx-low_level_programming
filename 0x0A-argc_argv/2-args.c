e8908294f34dd8cd0efe4264c4181488d4447c1d
Abdullahi Ibrahim <aismukaddas@gmail.com>
2022-10-05 18:10:42 -0700

    Mandatory Task

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-
 /**
- *  * main - prints all arguments it recieves
- *   * @argc: number of arguments passed to the function
- *    * @argv: argument vector of pointers to strings
- *     *
- *      * Return: always 0
- *       */
+ * main - prints all arguments it receives.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
 int main(int argc, char *argv[])
 {
-		int i;
+	int i;
 
-			for (i = 0; i < argc; i++)
-					{
-								printf("%s\n", argv[i]);
-									}
-				return (0);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
 }
